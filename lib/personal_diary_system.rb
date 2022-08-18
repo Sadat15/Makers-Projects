b51dd3ddbdada6f5dd50476c1783de01c671bd0b
Sadat <sadatmanjur15@gmail.com>
2022-08-18 00:05:23 +0000

    Exercise 1 and 2

--- lib/personal_diary_system.rb
+++ lib/personal_diary_system.rb
@@ -7,3 +7,20 @@ end
 def count_words(str)
   str.split(" ").count
 end
+
+def read_time(str)
+  word_count = count_words(str)
+  time_to_read = word_count/200.to_f
+  return time_to_read.ceil
+end
+
+def grammer_check(str)
+  if (str.chr == str.chr.upcase)
+    if (str[-1] == ".") || (str[-1] == "!") || (str[-1] == "?")
+    end 
+    return true
+  else
+    return false
+  end
+
+end
--- spec/personal_diary_system_spec.rb
+++ spec/personal_diary_system_spec.rb
@@ -9,6 +9,34 @@ RSpec.describe "make_snippet" do
   it "returns the number of words in the string" do
     result = count_words("Welcome to my personal diary. This is where I document my life.")
     expect(result).to eq 12
-  end  
+  end
+  
+  it "returns the estimated time taken to read string" do
+    result = read_time(" ") 
+    expect(result).to eq 0
+  end
+  
+  it "returns the estimated time taken to read string" do
+    result = read_time("One") 
+    expect(result).to eq 1
+  end
+  
+  it "returns false when string is empty" do
+    result = grammer_check(" ")
+    expect(result).to eq false
+  end
+  
+  it "returns false when string does not start with uppercase character" do
+    result = grammer_check("hello!")
+    expect(result).to eq false
+  end
+  
+  it "returns true when string starts with uppercase character and ends in question mark" do
+    result = grammer_check("Hello?")
+    expect(result).to eq true
+  end
+  
+  
+  
 end
   
